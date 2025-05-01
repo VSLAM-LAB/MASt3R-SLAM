a7bdfe2f5e0fa5eee7bba29198348da180b7b0b1
alejandrofontan <fontanvillacampa@gmail.com>
2025-05-01 17:10:45 +1000

    add pipy dependencies

--- thirdparty/in3d/setup.py
+++ thirdparty/in3d/setup.py
@@ -7,13 +7,13 @@ pyimgui_path = (thirdparty_path / "pyimgui").as_uri()
 setup(
     install_requires=[
         f"imgui @ {pyimgui_path}",
-        #"moderngl==5.12.0",
-        #"moderngl-window==2.4.6",
-        #"glfw",
-        #"pyglm",
-        #"msgpack",
-        #"numpy",
-        #"matplotlib",
-        #"trimesh[easy]",
+        "moderngl==5.12.0",
+        "moderngl-window==2.4.6",
+        "glfw",
+        "pyglm",
+        "msgpack",
+        "numpy",
+        "matplotlib",
+        "trimesh[easy]",
     ]
 )
